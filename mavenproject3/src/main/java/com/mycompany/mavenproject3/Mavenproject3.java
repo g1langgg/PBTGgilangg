607b924ca817400ead2532676ba70e461b19c0a9
g1langgg <145529684+g1langgg@users.noreply.github.com>
2024-08-28 09:33:14 +0700

    Pertemuan 3 soal 1

---
+++ mavenproject3/src/main/java/com/mycompany/mavenproject3/Mavenproject3.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mavenproject3;
+
+/**
+ *
+ * @author HP
+ */
+public class Mavenproject3 {
+
+    public static void main(String[] args) {
+       short angka;
+
+        angka = 9;
+        System.out.println(angka);
+
+        float f1 = 8.99f;
+        System.out.println(f1);
+
+        char cha = 'A';
+        System.out.println(cha);
+
+        boolean b = false;
+        System.out.println(b);
+
+        System.out.println("Hello World!");
+    }
+}
