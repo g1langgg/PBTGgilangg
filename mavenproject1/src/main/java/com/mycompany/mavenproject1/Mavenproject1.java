e304716e1ee3ea12bc0c9d2bd5edc8d3666f5cb4
g1langgg <145529684+g1langgg@users.noreply.github.com>
2024-08-28 09:13:50 +0700

    Pertemuan 2. Variable pada java

---
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mavenproject1;
+
+/**
+ *
+ * @author HP
+ */
+public class Mavenproject1 {
+
+    public static void main(String[] args) {
+        int a = 50;
+        float b = a;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
